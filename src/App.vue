589f00a31b513b6c675d60e08e207f68116126a1
Nadine Wischmeier <nadine.wischmeier@outlook.com>
2022-03-13 11:27:02 +0100

    add api

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1 class="text-center text-2xl text-red-400">Hello World</h1>
+  <h1 class="text-center text-2xl text-red-400">{{message}}</h1>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "",
+    };
+  },
+  async mounted() {
+    const { text } = await (await fetch("/api/message")).json();
+    this.message = text;
+  },
+};
+</script>
