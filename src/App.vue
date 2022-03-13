0571ff4a9480f9b864ac550d42a6f9556cbda9eb
Nadine Wischmeier <nadine.wischmeier@outlook.com>
2022-03-13 11:05:29 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-center text-2xl text-red-400">Hello World</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
